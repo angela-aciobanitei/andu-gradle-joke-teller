78cead80f23fbb066b7847e750cfcfd46f3bbda0
Angela Aciobanitei <aciobanitei.angela@gmail.com>
2019-09-06 01:17:53 +0100

    Use Java library in the GCE module

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.ang.acb.joketeller.JokeTeller;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -18,11 +19,12 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    /** A simple endpoint method that tells a joke */
+    @ApiMethod(name = "tellJoke")
+    public MyBean tellJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        JokeTeller jokeTeller = new JokeTeller();
+        response.setData(jokeTeller.tellJoke());
 
         return response;
     }
